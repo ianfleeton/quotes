19719493d026eb8f794aae692618abca31d557fd
Ian Fleeton <ianfleeton@gmail.com>
2009-06-22 17:21:50 +0100

    Basic actions for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,54 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all :order => :position
+  end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def create
+    @item = Item.new(params[:item])
+    
+    if @item.save
+      flash[:notice] = 'Item created.'
+      redirect_to items_path
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(params[:item])
+      flash[:notice] = 'Item saved.'
+      redirect_to items_path
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    flash[:notice] = 'Item deleted.'
+    redirect_to items_url
+  end
+
+  def up
+    @item = Item.find(params[:id])
+    @item.move_higher
+    redirect_to items_url
+  end
+  
+  def down
+    @item = Item.find(params[:id])
+    @item.move_lower
+    redirect_to items_url
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ActiveRecord::Base
   acts_as_list
+
+  def price_in_pounds
+    sprintf("%.2f", price.to_f / 100.0) unless price.nil?
+  end
+
+  def price_in_pounds= price_in_pounds
+    self.price = price_in_pounds.to_f * 100
+  end
 end
