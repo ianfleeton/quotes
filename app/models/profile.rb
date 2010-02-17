2d975470224c355f8f8f69c039788e3dfc49a27e
Ian Fleeton <ianfleeton@gmail.com>
2010-02-17 12:25:10 +0000

    Validate presence of name, html, from and subject for profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   has_many :categories, :order => 'position'
   has_many :items
   has_many :quotes, :conditions => 'sent_at IS NOT NULL', :order => 'created_at DESC'
-  validates_presence_of :domain
+  validates_presence_of :domain, :name, :html, :from, :subject
   validates_uniqueness_of :domain
 end
