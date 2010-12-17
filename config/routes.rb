64bc61ec8be4f3e349327b8ee6bb6f61f7b1dd1d
Ian Fleeton <ianfleeton@gmail.com>
2010-12-17 13:52:42 +0000

    Routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -26,4 +26,21 @@ Quotes::Application.routes.draw do
 
   resources :profiles
 
+  resources :users
+
+  resource :session, :only => [:new, :create, :destroy]
+
+  match 'signup' => 'users#new', :as => :signup
+
+  match 'register' => 'users#create', :as => :register
+
+  match 'login' => 'sessions#new', :as => :login
+
+  match 'logout' => 'sessions#destroy', :as => :logout
+
+  match 'forgot_password' => 'users#forgot_password', :as => :forgot_password
+  match 'forgot_password_send' => 'users#forgot_password_send', :as => :forgot_password_send
+  match 'reset_password' => 'users#forgot_password_new', :as => :reset_password
+  match 'forgot_password_change' => 'users#forgot_password_change'
+
 end
