fe7a32d71420dbd15328ea28d161d1c8926567ea
Ian Fleeton <ianfleeton@gmail.com>
2020-12-23 17:36:19 +0000

    Validate presence of intro (required in db)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,6 @@ class Profile < ActiveRecord::Base
   has_many :categories, -> { order 'position' }
   has_many :items
   has_many :quotes, -> { where('sent_at IS NOT NULL').order('created_at DESC') }
-  validates_presence_of :domain, :name, :html, :from, :subject
+  validates_presence_of :domain, :name, :html, :intro, :from, :subject
   validates_uniqueness_of :domain
 end
